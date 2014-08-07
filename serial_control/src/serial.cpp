78308bbbfd6f43cbf8177c0a9e8fc91e2170dfc8
aqua <lanyusea@gmail.com>
2014-08-08 00:03:49 +0800

    serial init finish

--- serial_control/src/serial.cpp
+++ serial_control/src/serial.cpp
@@ -6,8 +6,13 @@
 using namespace LibSerial;
 using namespace std;
 
+SerialStream serial_port;
+const int BUF_LEN=44;  //TODO
+const int MSG_LEN=BUF_LEN/4;
+char msgArray[MSG_LEN];
+
 void callback(const tld_msgs::BoundingBox &data) {
-    //receive msg
+    //receive msg test
     //ROS_INFO("I heared the msg");
     //ROS_INFO("=======detect data=======");
     //ROS_INFO("Center Position: (%d,%d)",data.x,data.y);
@@ -23,10 +28,82 @@ void callback(const tld_msgs::BoundingBox &data) {
 
     //send the angle using serial port
 
+
+    //serial_port<< "AA"
+    //    <<  <<
+    //    <<  <<
+    //    <<  <<
+    //    <<  <<
+    //    <<"BB";
+
 }
 
 int main(int argc, char** argv)
 {
+
+    //serial_port init part
+    //credit to Paul Yang and his team
+    serial_port.Open("/dev/ttyUSB");//TODO
+    if ( ! serial_port.good() )
+    {
+        std::cerr << "[" << __FILE__ << ":" << __LINE__ << "] "
+            << "Error: Could not open serial port."
+            << std::endl ;
+        exit(1) ;
+    }
+
+    //
+    // Set the baud rate of the serial port.
+    //
+    serial_port.SetBaudRate( SerialStreamBuf::BAUD_115200 ) ;
+    if ( ! serial_port.good() )
+    {
+        std::cerr << "Error: Could not set the baud rate." <<
+            std::endl ;
+        exit(1) ;
+    }
+    //
+    // Set the number of data bits.
+    //
+    serial_port.SetCharSize( SerialStreamBuf::CHAR_SIZE_8 ) ;
+    if ( ! serial_port.good() )
+    {
+        std::cerr << "Error: Could not set the character size." <<
+            std::endl ;
+        exit(1) ;
+    }
+    //
+    // Disable parity.
+    //
+    serial_port.SetParity( SerialStreamBuf::PARITY_NONE ) ;
+    if ( ! serial_port.good() )
+    {
+        std::cerr << "Error: Could not disable the parity." <<
+            std::endl ;
+        exit(1) ;
+    }
+    //
+    // Set the number of stop bits.
+    //
+    serial_port.SetNumOfStopBits( 1 ) ;
+    if ( ! serial_port.good() )
+    {
+        std::cerr << "Error: Could not set the number of stop bits."
+            << std::endl ;
+        exit(1) ;
+    }
+    //
+    // Turn off hardware flow control.
+    //
+    serial_port.SetFlowControl( SerialStreamBuf::FLOW_CONTROL_NONE ) ;
+    if ( ! serial_port.good() )
+    {
+        std::cerr << "Error: Could not use hardware flow control."
+            << std::endl ;
+        exit(1) ;
+    }
+
+    //ros init part
     ros::init(argc,argv,"serialNode");
     ros::NodeHandle n;
     tld_msgs::BoundingBox newMsg;
