93e685477fb0022146470ebf974098f37bd2e0fd
aqua <lanyusea@gmail.com>
2014-08-12 13:21:20 +0800

    code update

---
+++ CMakeFiles/color_detect.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/color_detect.dir/video.cpp.o"
+  "color_detect.pdb"
+  "color_detect"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/color_detect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ video.h
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <opencv2/legacy/legacy.hpp>
+#include <stack>
+#include <queue>
+#include <ros/ros.h>
+#include <opencv2/opencv.hpp>//TODO
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc_c.h>
+#include <SerialStream.h>
+using namespace std;
+
+#define Fx 817.74458
+#define Fy 818.04921
+#define Cx 314.85848
+#define Cy 227.90983
+#define K1 0.02061
+#define K2 -0.29551
+#define P1 0.00385
+#define P2 0.00028
+
+
+struct SObstacle
+{
+    CvPoint FrPt;
+    CvPoint NrPt;
+    CvPoint CnPt;
+    int nWidth;
+    int nHeight;
+    float fDist;
+    float fHAngle;
+    float fVAngle;
+    float fVx;
+    float fVy;
+};
+
